092af5a4aee95ed0fe91d388e9c6811c1f87f81a
Rodrigo Lira <rodrigocliras@gmail.com>
2021-09-16 22:56:02 -0300

    Exemplo de Projeto usando Structs

---
+++ aula04/ExemploStructs/ExemploStructs/Entities/Structs/Cords.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploStructs.Entities.Structs
+{
+    public struct Coords
+    {
+        public Coords(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; init; }
+        public double Y { get; init; }
+
+        public override string ToString() => $"({X}, {Y})";
+    }
+}
