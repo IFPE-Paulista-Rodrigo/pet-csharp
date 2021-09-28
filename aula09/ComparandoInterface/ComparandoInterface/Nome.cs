3a0198080b98b5097bf7f0698e37acb2006eb0cc
Rodrigo Lira <rodrigocliras@gmail.com>
2021-09-28 19:47:44 -0300

    Exemplo de Interface

---
+++ aula09/ComparandoInterface/ComparandoInterface/Nome.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparandoInterface
+{
+    class Pessoa : IComparable
+    {
+        public string Nome { get; set; }
+        public int CompareTo(object other)
+        {
+            if (other == null) return 1;
+            Pessoa otherObj = other as Pessoa;
+            if (otherObj != null)
+                return this.Nome.CompareTo(otherObj.Nome);
+            else
+                throw new ArgumentException("Object is not a Pessoa");
+        }
+    }
+}
---
+++ aula09/ComparandoInterface/ComparandoInterface/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ComparandoInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Exemplo usando Pessoa
+            Pessoa[] pessoas = new Pessoa[3];
+            pessoas[0] = new Pessoa() { Nome = "Rodrigo" };
+            pessoas[1] = new Pessoa() { Nome = "Abel" };
+            pessoas[2] = new Pessoa() { Nome = "Berenice" };
+            
+            Console.WriteLine("Ordenado os objetos de pessoa");
+            MySort(pessoas);
+            foreach (Pessoa pessoa in pessoas)
+            {
+                Console.WriteLine(pessoa.Nome);
+            }
+
+
+            // Exemplo usando Temperature
+            Temperature[] temperaturas = new Temperature[5];
+            temperaturas[0] = new Temperature(20);
+            temperaturas[1] = new Temperature(2);
+            temperaturas[2] = new Temperature(-20);
+            temperaturas[3] = new Temperature(33);
+            temperaturas[4] = new Temperature(89);
+            
+            Console.WriteLine("Ordenado a temperatura");
+            MySort(temperaturas);
+            foreach (Temperature temperatura in temperaturas)
+            {
+                Console.WriteLine(temperatura.Celsius.ToString("F2"));
+            }
+        }
+
+
+        static void MySort(IComparable[] array)
+        {
+            for(int i = 0; i < array.Length; i += 1)
+            {
+                for (int j = 0; j < array.Length; j += 1)
+                {
+                    if (array[i].CompareTo(array[j]) <= -1)
+                    {
+                        IComparable temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+
+                    }
+                }
+            }
+
+
+        }
+    }
+}
