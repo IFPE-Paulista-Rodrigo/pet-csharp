a97d2c343bf5ed4473517ca3a2718430965e33b9
Rodrigo Lira <rodrigocliras@gmail.com>
2021-10-02 13:31:03 -0300

    Exemplo mudando a cor das letras (foreground)

---
+++ projeto/ExemplosProjeto/ExemplosProjeto/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ExemplosProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChangingConsoleColors();
+        }
+
+        public static void ChangingConsoleColors()
+        {
+            //Documentation
+            https://docs.microsoft.com/pt-br/dotnet/api/system.console.foregroundcolor?view=net-5.0
+
+            // Display current Foreground color
+            Console.WriteLine("Cor Padrão do Foreground: {0}",
+                                     Console.ForegroundColor);
+
+            //store default color
+            ConsoleColor defaultColor = Console.ForegroundColor;
+
+            // Set the Foreground color to blue
+            Console.ForegroundColor
+                = ConsoleColor.Blue;
+
+            // Display current Foreground color
+            Console.WriteLine("Mudando a cor do Foreground: {0}",
+                                    Console.ForegroundColor);
+
+            //get all colors
+            ConsoleColor[] consoleColors = (ConsoleColor[])ConsoleColor
+              .GetValues(typeof(ConsoleColor));
+
+            //Choose a random color for each char
+            Random rand = new Random();
+            int randPos = -1;
+            foreach (var character in "Um texto qualquer".ToUpper())
+            {
+                randPos = rand.Next(0, consoleColors.Length - 1);
+                Console.ForegroundColor = consoleColors[randPos];
+                Console.Write(character);
+            }
+
+            //backing to normal
+            Console.ForegroundColor = defaultColor;
+            Console.WriteLine("\nVoltando a cor normal");
+
+        }
+
+    }
+}
